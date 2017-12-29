1a86caa01ff4f32a83bab56d36f0bbc4ba748dff
jzk <893721871@qq.com>
2017-12-29 14:47:15 +0800

    zxing  扫描

---
+++ app/src/main/java/com/timi/qrcodeapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.timi.qrcodeapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.timi.zxingscanlibrary.CommonScanActivity;
+import com.timi.zxingscanlibrary.utils.Constant;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            //权限还没有授予，需要在这里写申请权限的代码
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 60);
+        } else {
+            //权限已经被授予，在这里直接写要执行的相应方法即可
+            Intent intent = new Intent(this, CommonScanActivity.class);
+            intent.putExtra(Constant.REQUEST_SCAN_MODE, Constant.REQUEST_SCAN_MODE_ALL_MODE);
+            startActivityForResult(intent, 0);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+                Toast.makeText(MainActivity.this, bundle.getString("result"), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
